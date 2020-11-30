a0728933b2e7382b1e43ebebf52fd9013a203bce
Andrew Mozharovsky <kainnet.job@gmail.com>
2020-11-30 18:15:39 +0200

    30-11-work

---
+++ js/mobail-menu.js
@@ -0,0 +1,18 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+      menu: document.querySelector('[data-menu]')      
+      };
+    
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+      refs.menu.classList.toggle('is-hidden');
+      }
+    
+  })();
